01b8b5a339173e74eaf35d7fcf04118d7b00a7cb
marvinick <marviluw@gmail.com>
2015-09-18 08:26:05 -0700

    this commit will have README and inital setup

---
+++ spec/specs.js
@@ -0,0 +1,27 @@
+// describe('Contact', function() {
+//   it("creates a new contact with the given specifications", function() {
+//     var testContact = new Contact("Rita","Moreno");
+//     expect(testContact.firstName).to.equal("Rita");
+//     expect(testContact.lastName).to.equal("Moreno");
+//     expect(testContact.addresses).to.eql([]);
+//   });
+
+//   it("adds the fullName method to all contacts", function() {
+//     var testContact = new Contact("Sherlock","Holmes");
+//     expect(testContact.fullName()).to.equal("Sherlock Holmes");
+//   });
+// });
+
+// describe('Address', function() {
+//   it("create a new address with given specifications", function() {
+//     var testAddress = new Address("123 Main St", "Test City","Test State");
+//     expect(testAddress.street).to.equal("123 Main St");
+//     expect(testAddress.city).to.equal("Test City");
+//     expect(testAddress.state).to.equal("Test State");
+//   });
+
+//   it("adds the fulladdress method to all addresses", function() {
+//     var testAddress = new Address("123 Main St", "Test City","Test State");
+//     expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
+//   })
+// })
